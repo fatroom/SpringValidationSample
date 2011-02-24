fc3874d56d3fa1e78d271937c4cea12bfd096c66
Roman Romanchuk <fatroom@gmail.com>
2011-02-25 00:56:48 +0200

    Mock implementation of store method added

--- src/main/java/org/noisyteam/samples/spring/validation/controller/PersonController.java
+++ src/main/java/org/noisyteam/samples/spring/validation/controller/PersonController.java
@@ -2,6 +2,7 @@ package org.noisyteam.samples.spring.validation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
@@ -10,10 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class PersonController {
+    private static final String DEFAULT_VIEW = "WEB-INF/views/home.jsp";
 
     @RequestMapping(value = "/")
     public String home() {
-        System.out.println("HomeController: Passing through...");
-        return "WEB-INF/views/home.jsp";
+        System.out.println("PersonController: Passing through...");
+        return DEFAULT_VIEW;
+    }
+
+    @RequestMapping(value = "/", method=RequestMethod.PUT)
+    public String create() {
+        System.out.println("PersonController: Saving action");
+        return DEFAULT_VIEW;
     }
 }
